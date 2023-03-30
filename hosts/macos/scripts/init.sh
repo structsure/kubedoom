7a7c0a108cfa0ab3efaa3c3012520f87a557413a
Christopher Melgard <chris.melgard@braingu.com>
2023-03-30 16:48:54 -0500

    change init to dump ib_creds into root

--- hosts/macos/scripts/init.sh
+++ hosts/macos/scripts/init.sh
@@ -5,7 +5,7 @@ brew install minikube kubectl hyperkit helm yq fluxcd/tap/flux ripgrep k9s
 
 yq -n ".registryCredentials.username = $(security find-generic-password -s registry1.dso.mil |rg '.*acc.*(".*")$' -r '$1') \
       |.registryCredentials.password = \"$(security find-generic-password -s registry1.dso.mil -w)\"" \
-> clusters/minikube/bigbang-values/secrets/ib_creds.yaml
+> ib_creds.yaml
 
 echo -----
 echo cheking livelness of your harbor api creds
